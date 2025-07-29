4b7d1a53b0005c8c6d4c4494880c90be3e0a738b
SPROESSERArthur <arthursproesser@gmail.com>
2025-07-29 10:55:25 +0200

    update to use cluster

--- bin/twin.py
+++ bin/twin.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
+
 import numpy as np, os, sys
 
 from pathlib import Path
 
-fo = str(Path(os.path.dirname(os.path.realpath(__file__))).parent) + "/src"
-if fo not in sys.path:
-    sys.path.append(os.path.abspath(fo))
+# fo = str(Path(os.path.dirname(os.path.realpath(__file__))).parent) + "/src"
+# if fo not in sys.path:
+#     sys.path.append(os.path.abspath(fo))
+
+#
+# adiciona automaticamente a pasta src/ (ao lado de bin/) ao PYTHONPATH,
+# funcione onde for, seja local ou no cluster
+#
+this_file = Path(__file__).resolve()
+project_root = this_file.parent.parent     # twinTrans/
+src_dir      = project_root / "src"
+if src_dir.is_dir() and str(src_dir) not in sys.path:
+    sys.path.insert(0, str(src_dir))
+
 import param_var as pv, functions as f
 
 from warnings import filterwarnings
